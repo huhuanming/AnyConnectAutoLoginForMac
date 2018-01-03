cfdb84b0825d2272a58c2e6d91ff0467b186bf0f
huhuanming <workboring@gmail.com>
2018-01-04 00:43:31 +0800

    change function name

--- index.js
+++ index.js
@@ -3,46 +3,45 @@ const fs = require('fs')
 const execSync = require('child_process').execSync
 
 const setting = JSON.parse(fs.readFileSync('./setting.json'))
-
-const callAppleScript = command => execSync(`osascript -l JavaScript -e "${command}"`)
-const sleepAWhile = (seconds) => execSync(`sleep ${seconds || Math.random() * 2}`)
-
-// keycode 36 is ⏎
-const pressEnterKey = () => callAppleScript(`${systemEvents}.keyCode(36)`)
 const clientName = "Application('com.cisco.Cisco-AnyConnect-Secure-Mobility-Client')"
 const systemEvents = "Application('System Events')"
 
+const execScript = command => execSync(`osascript -l JavaScript -e "${command}"`)
+const sleep = (seconds) => execSync(`sleep ${seconds || Math.random() * 2}`)
+// keycode 36 is ⏎
+const pressEnterKey = () => execScript(`${systemEvents}.keyCode(36)`)
+
 const checkCiscoExsit = () => 
-    callAppleScript(
+    execScript(
         `${systemEvents}.processes().filter(process => process.name() === 'Cisco AnyConnect Secure Mobility Client')`
     ).toString('utf8').length > 10
 
 while(checkCiscoExsit()) {
   try {
-    callAppleScript(`${clientName}.quit()`)
+    execScript(`${clientName}.quit()`)
   } catch (error) {
-    sleepAWhile(4)
+    sleep(4)
   }
 }
 
-callAppleScript(`${clientName}.activate()`)
-sleepAWhile()
+execScript(`${clientName}.activate()`)
+sleep()
 
-callAppleScript(`${systemEvents}.keystroke('${setting.vpnName}')`)
+execScript(`${systemEvents}.keystroke('${setting.vpnName}')`)
 pressEnterKey()
-sleepAWhile(2)
+sleep(2)
 
-callAppleScript(`${systemEvents}.keystroke('${setting.password}')`)
+execScript(`${systemEvents}.keystroke('${setting.password}')`)
 pressEnterKey()
-sleepAWhile(2)
+sleep(2)
 
-callAppleScript(`${systemEvents}.keystroke('${setting.authenticationMethod}')`)
+execScript(`${systemEvents}.keystroke('${setting.authenticationMethod}')`)
 pressEnterKey()
-sleepAWhile(2)
+sleep(2)
 
 const token = speakeasy.time({
     secret: setting.secretKey, 
     encoding: 'base32', 
 })
-callAppleScript(`${systemEvents}.keystroke('${token}')`)
+execScript(`${systemEvents}.keystroke('${token}')`)
 pressEnterKey()

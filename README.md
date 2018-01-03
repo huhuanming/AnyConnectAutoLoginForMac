26c958b7ae63964b3c814cf60644b2c3e7af4efe
huhuanming <workboring@gmail.com>
2018-01-03 23:29:35 +0800

    init

---
+++ README.md
@@ -0,0 +1,13 @@
+# How To Use
+
+At first you should fill data into setting.json
+
+```shell
+cp setting.json.example cp setting.json
+```
+
+then enjoy it!
+
+```shell
+node index.js
+```
---
+++ index.js
@@ -0,0 +1,38 @@
+const speakeasy = require('speakeasy')
+const fs = require('fs')
+const execSync = require('child_process').execSync;
+
+const setting = JSON.parse(fs.readFileSync('./setting.json'))
+
+const callAppleScript = command => execSync(`osascript -l JavaScript -e "${command}"`)
+const sleepAWhile = (seconds) => execSync(`sleep ${seconds || Math.random() * 2}`)
+
+// keycode 36 is ⏎
+const pressEnterKey = () => callAppleScript(`${systemEvents}.keyCode(36)`)
+
+const clientName = "Application('com.cisco.Cisco-AnyConnect-Secure-Mobility-Client')"
+const systemEvents = "Application('System Events')"
+
+callAppleScript(`${clientName}.activate()`)
+sleepAWhile()
+
+callAppleScript(`${systemEvents}.keystroke('${setting.vpnName}')`)
+pressEnterKey()
+sleepAWhile(2)
+
+callAppleScript(`${systemEvents}.keystroke('${setting.password}')`)
+sleepAWhile()
+pressEnterKey()
+sleepAWhile(2)
+
+callAppleScript(`${systemEvents}.keystroke('${setting.authenticationMethod}')`)
+sleepAWhile()
+pressEnterKey()
+sleepAWhile(2)
+
+const token = speakeasy.time({
+    secret: setting.secretKey, 
+    encoding: 'base32', 
+})
+callAppleScript(`${systemEvents}.keystroke('${token}')`)
+pressEnterKey()

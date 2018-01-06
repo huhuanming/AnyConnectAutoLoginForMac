ab2931e259679fdaf0f45e8d73b27e95286d5412
huhuanming <workboring@gmail.com>
2018-01-06 17:18:11 +0800

    style: fix template style

--- template.js
+++ template.js
@@ -4,7 +4,7 @@ const password = "PASSWORD"
 const authenticationMethod = "AUTHENTICATION_METHOD"
 const token = "ACCESS_TOKEN"
 
-var app = Application("System Events").applicationProcesses.byName("Cisco AnyConnect Secure Mobility Client")
+const app = Application("System Events").applicationProcesses.byName("Cisco AnyConnect Secure Mobility Client")
 
 while(!app.exists()) {
 	Application("com.cisco.Cisco-AnyConnect-Secure-Mobility-Client").activate()
@@ -13,10 +13,10 @@ while(!app.exists()) {
 
 delay(2 * Math.random())
 
-var windows = app.windows
-var vpnWindow = windows.byName("Cisco AnyConnect | " + vpnName)
+const windows = app.windows
+const vpnWindow = windows.byName("Cisco AnyConnect | " + vpnName)
+const reTry = [0, 0, 0, 0]
 
-var reTry = [0, 0, 0, 0]
 while (!windows.at(1).buttons.byName("Disconnect").exists() && !(reTry[0] > 3 || reTry[1] > 3 || reTry[2] > 3 || reTry[3] > 3)) {
   if (vpnWindow.exists()) {
     if (vpnWindow.staticTexts.byName("UserName:").exists() && vpnWindow.staticTexts.byName("Password:").exists()) {
@@ -25,24 +25,24 @@ while (!windows.at(1).buttons.byName("Disconnect").exists() && !(reTry[0] > 3 ||
       vpnWindow.buttons.byName("OK").click()
       reTry[0] += 1
 	}
-		if (vpnWindow.scrollAreas.at(0).textAreas.at(0).exists()) {
-			if (vpnWindow.scrollAreas.at(0).textAreas.at(0).value().includes("Please select your second authentication method [num]:")) {
-					vpnWindow.textFields.at(0).value = authenticationMethod
-						vpnWindow.buttons.byName("Continue").click()
-						reTry[1] += 1
-			}
-			
-			if (vpnWindow.scrollAreas.at(0).textAreas.at(0).value().includes("Enter the code for ")) {
-				vpnWindow.textFields[0].value = token
+	if (vpnWindow.scrollAreas.at(0).exists() && vpnWindow.scrollAreas.at(0).textAreas.at(0).exists()) {
+		if (vpnWindow.scrollAreas.at(0).textAreas.at(0).value().includes("Please select your second authentication method [num]:")) {
+			vpnWindow.textFields.at(0).value = authenticationMethod
 			vpnWindow.buttons.byName("Continue").click()
-					reTry[2] += 1
-			}
+			reTry[1] += 1
 		}
+		
+		if (vpnWindow.scrollAreas.at(0).textAreas.at(0).value().includes("Enter the code for ")) {
+			vpnWindow.textFields[0].value = token
+			vpnWindow.buttons.byName("Continue").click()
+			reTry[2] += 1
+		}
+	}
   } else {
-	  windows.at(1).buttons.byName("Connect").click()
+	windows.at(1).buttons.byName("Connect").click()
     reTry[3] += 1
   }
-	delay(2 * Math.random())
+  delay(2 * Math.random())
 }
 
 windows[0].entireContents()
